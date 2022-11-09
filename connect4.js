6696799acb4a7d70d0c4637b3777970ebfeaa3ef
David A. Salazar <davidalonzso@gmail.com>
2022-11-09 15:50:55 -0800

    add connect4.js

--- connect4.js
+++ connect4.js
@@ -82,6 +82,8 @@ function findSpotForCol(x) {
 
 function placeInTable(y, x) {
     // TODO: make a div and insert into correct table cell
+    let piece = document.createElement('div');
+    piece.classList.add('piece', `p${currPlayer}`);
 }
 
 /** endGame: announce game end */
